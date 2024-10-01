cbe33c60d8352227632982831b88fc0eca6eb985
Kriv Jhajhria <105179164+Glitchood@users.noreply.github.com>
2024-09-30 17:24:38 -0700

    🎉 Basic Functionality

---
+++ scripts/script.js
@@ -0,0 +1,152 @@
+//global variables
+let displayValue = "";
+let firstNumber = "";
+let secondNumber = "";
+let firstOperator = "";
+let secondOperator = "";
+let firstNumberCheck = false;
+let lastOperatorEqualCheck = false;
+let decimalDotCheck = false;
+
+function add(num1, num2) {
+    return Math.round((num1 + num2) * 100) / 100;
+}
+
+function subtract(num1, num2) {
+    return Math.round((num1 - num2) * 100) / 100;
+}
+
+function multipy(num1, num2) {
+    return Math.round((num1 * num2) * 100) / 100;
+}
+
+function divide(num1, num2) {
+    return Math.round((num1 / num2) * 100) / 100;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        case "-":
+            return subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            return multipy(firstNumber, secondNumber);
+            break;
+        case "/":
+            if (secondNumber === 0) {
+                return "Well well well seems like you tried dividing by 0: ERROR"
+            }
+            return divide(firstNumber, secondNumber);
+            break;
+    }
+}
+
+const display = document.querySelector("#display");
+
+const numberButtons = document.querySelectorAll(".numberButtons");
+function getNumberInput(numberButtons) {
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            displayValue += event.target.textContent;
+            setDisplay(displayValue);
+        })
+    })
+}
+getNumberInput(numberButtons);
+
+const decimalDot = document.querySelector("#dot");
+function getDecimalDotInput(decimalDot) {
+    decimalDot.addEventListener("click", (event) =>{
+        if (!decimalDotCheck) {
+            decimalDotCheck = true;
+            displayValue += event.target.textContent;
+            setDisplay(displayValue);
+        }
+    })
+}
+getDecimalDotInput(decimalDot);
+
+const operatorButtons = document.querySelectorAll(".operatorButtons");
+function getOperatorInput(operatorButtons) {
+    operatorButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            //If a firstNumber hasn't been assigned, we can assign the displayValue as firstNumber
+            if (!firstNumberCheck) {
+                setFirstNumber(displayValue);
+                firstNumberCheck = true;
+                //Store the operator in the firstOperator variable since it is used when another operator button was pressed
+                firstOperator = event.target.textContent;
+            }
+            else if (lastOperatorEqualCheck) {
+                //Keep the operator, but don't do any calculations
+                firstOperator = event.target.textContent;
+                lastOperatorEqualCheck = false;
+            }
+            else {
+                //If firstNumber is assigned we can assign the display Value to the secondNumber
+                setSecondNumber(displayValue);
+                //Store the second operator in it's own variable to not overwrite the first input aswell need it when equals is pressed
+                secondOperator = event.target.textContent;
+                //Calculate and display the numbers
+                setDisplay(operate(firstNumber, secondNumber, firstOperator));
+                //Set result as first number so we can continue with the next calculation
+                firstNumber = operate(firstNumber, secondNumber, firstOperator);
+                //Set first operator so when we get the next operator we calculate with the right one
+                firstOperator = secondOperator;
+            }
+            displayValue = "";
+            decimalDotCheck = false;
+        })
+    });
+}
+getOperatorInput(operatorButtons);
+
+const equalButton = document.querySelector("#equals");
+function getEqualInput(equalButton) {
+    equalButton.addEventListener("click", () => {
+        setSecondNumber(displayValue);
+        if (secondOperator.length > 0 && !lastOperatorEqualCheck) {
+            setDisplay(operate(firstNumber, secondNumber, secondOperator));
+            firstNumber = operate(firstNumber, secondNumber, firstOperator);
+            //Check for firstNumber so if equal is pressed before nothing happens
+        } else if (firstNumberCheck && !lastOperatorEqualCheck) {
+            setDisplay(operate(firstNumber, secondNumber, firstOperator));
+            firstNumber = operate(firstNumber, secondNumber, firstOperator);
+        }
+        lastOperatorEqualCheck = true;
+        decimalDotCheck = false;
+    })
+}
+getEqualInput(equalButton);
+
+
+
+const clearButton = document.querySelector("#clear");
+function clearPress(params) {
+    clearButton.addEventListener("click", () => {
+        displayValue = "";
+        firstNumber = "";
+        secondNumber = "";
+        firstOperator = "";
+        secondOperator = "";
+        firstNumberCheck = false;
+        decimalDotCheck = false;
+        setDisplay("0");
+    })
+}
+clearPress();
+
+function setFirstNumber(num) {
+    firstNumber = parseFloat(num);
+}
+
+function setSecondNumber(num) {
+    secondNumber = parseFloat(num);
+}
+
+function setDisplay(displayValue) {
+    display.textContent = displayValue;
+}
